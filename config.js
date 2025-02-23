940ff47c1907820b3b3cfd521260724cedb434d4
Steve Phelps <79sphelps@gmail.com>
2025-02-23 09:07:54 -0800

    More cleanup

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// server/config.js
 module.exports = {
   AUTH0_DOMAIN: "sphelps.auth0.com", // e.g., kmaida.auth0.com
   // AUTH0_API_AUDIENCE: "http://localhost:8081/api/",
@@ -9,7 +8,6 @@ module.exports = {
     process.env.MONGO_URI ||
     // "mongodb://sphelps:sphelps1@ds119052.mlab.com:19052/all-about-hair",
     // "mongodb://localhost:27017/all-about-hair",
-    // "mongodb+srv://79sphelps:Stritone&1@cluster0.po6aaqm.mongodb.net/all-about-hair?retryWrites=true&w=majority",
     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.po6aaqm.mongodb.net/all-about-hair?retryWrites=true&w=majority`,
   NAMESPACE: "http://myapp.com/roles" // from Auth0.com rules specification
 };
